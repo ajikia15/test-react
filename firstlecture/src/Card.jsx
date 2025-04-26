ab8f43a90d4d1ac0d097f130958dbd6688f8038c
ajikia15 <aleksandrej15@gmail.com>
2025-04-26 18:11:33 +0400

    initialization

---
+++ firstlecture/src/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <img
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fresidenceplaza.ge%2Fwp-content%2Fuploads%2F2024%2F04%2Fshaurma-scaled.jpg&f=1&nofb=1&ipt=5c2e4fd26abae806c2c44ff7d141708a182ae8c7f714ce2e0646e6622db5a866"
+        alt=""
+        className="card-pic"
+      />
+      <p className="title">Black Shaurma</p>
+      <p>50$</p>
+    </div>
+  );
+}
